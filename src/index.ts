daba6c2eeec0c519d9bbc58b56e7bd8be5d030f5
shirayuki <shirayuki@46ki75.com>
2024-02-11 20:28:24 +0900

    feat(build): configure project for npm publication

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import { notion } from './Client'
-
-void (async () => {
-  try {
-    const id = String(process.env.NOTION_PAGE_ID)
-    const res = await notion.blocksChildren(id)
-    const data = await res.toHTML()
-    console.dir(data, { depth: null })
-  } catch (error) {
-    console.error(error)
-  }
-})()
+export * from './Client'
+export * from './block'
+export * from './other'
