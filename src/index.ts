3571e5ac51a5d1e8c808c1babcaf4034dfd8acda
shirayuki <shirayuki@46ki75.com>
2024-02-17 13:32:27 +0900

    refactor: remove unnecessary test code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { notion } from './Client'
 export * from './Client'
 export * from './block'
 export * from './other'
-void (async () => {
-  const a = await notion.getHTML('57ca7911c60441b499d1fded6cb14b2f')
-  console.log(a)
-})()
